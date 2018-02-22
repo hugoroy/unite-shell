685bbfed73900533d54ddd1b882c7669583cb601
Hugo Roy <hugo@hugoroy.eu>
2018-02-22 16:36:13 +0100

    deactivate some features

--- unite@hardpixel.eu/extension.js
+++ unite@hardpixel.eu/extension.js
@@ -3,11 +3,11 @@ const Main             = imports.ui.main;
 const ExtensionUtils   = imports.misc.extensionUtils;
 const Unite            = ExtensionUtils.getCurrentExtension();
 const ActivateWindow   = Unite.imports.modules.activateWindow.ActivateWindow;
-const ExtendLeftBox    = Unite.imports.modules.extendLeftBox.ExtendLeftBox;
-const MessageTray      = Unite.imports.modules.messageTray.MessageTray;
-const ActivitiesButton = Unite.imports.modules.activitiesButton.ActivitiesButton;
-const AppMenu          = Unite.imports.modules.appMenu.AppMenu;
-const WindowButtons    = Unite.imports.modules.windowButtons.WindowButtons;
+// const ExtendLeftBox    = Unite.imports.modules.extendLeftBox.ExtendLeftBox;
+// const MessageTray      = Unite.imports.modules.messageTray.MessageTray;
+// const ActivitiesButton = Unite.imports.modules.activitiesButton.ActivitiesButton;
+// const AppMenu          = Unite.imports.modules.appMenu.AppMenu;
+// const WindowButtons    = Unite.imports.modules.windowButtons.WindowButtons;
 const WindowDecoration = Unite.imports.modules.windowDecoration.WindowDecoration;
 const TopIcons         = Unite.imports.modules.topIcons.TopIcons;
 
@@ -16,11 +16,11 @@ var UniteShell = new Lang.Class({
 
   _init: function() {
     this._activateWindow   = new ActivateWindow();
-    this._extendLeftBox    = new ExtendLeftBox();
-    this._messageTray      = new MessageTray();
-    this._activitiesButton = new ActivitiesButton();
-    this._appMenu          = new AppMenu();
-    this._windowButtons    = new WindowButtons();
+    // this._extendLeftBox    = new ExtendLeftBox();
+    // this._messageTray      = new MessageTray();
+    // this._activitiesButton = new ActivitiesButton();
+    // this._appMenu          = new AppMenu();
+    // this._windowButtons    = new WindowButtons();
     this._windowDecoration = new WindowDecoration();
     this._topIcons         = new TopIcons();
 
@@ -29,11 +29,11 @@ var UniteShell = new Lang.Class({
 
   destroy: function() {
     this._activateWindow.destroy();
-    this._extendLeftBox.destroy();
-    this._messageTray.destroy();
-    this._activitiesButton.destroy();
-    this._appMenu.destroy();
-    this._windowButtons.destroy();
+    // this._extendLeftBox.destroy();
+    // this._messageTray.destroy();
+    // this._activitiesButton.destroy();
+    // this._appMenu.destroy();
+    // this._windowButtons.destroy();
     this._windowDecoration.destroy();
     this._topIcons.destroy();
 
